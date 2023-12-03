19178e2f7ca1d5295e42beb08e79321f26e788a0
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-12-02 20:09:03 -0500

    New Update

--- utils/redis.js
+++ utils/redis.js
@@ -1,54 +1,55 @@
 import { createClient } from 'redis';
+
 const util = require('util');
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
-        this.client.getAsync = util.promisify(this.client.get).bind(this.client);
-        this.client.setExAsync = util.promisify(this.client.setex).bind(this.client);
-        this.client.delAsync = util.promisify(this.client.del).bind(this.client);
+  constructor() {
+    this.client = createClient();
+    this.client.getAsync = util.promisify(this.client.get).bind(this.client);
+    this.client.setExAsync = util.promisify(this.client.setex).bind(this.client);
+    this.client.delAsync = util.promisify(this.client.del).bind(this.client);
 
-        this.client.on('error', (err) => {
-            console.error(`Redis client not connected to the server: ${err}`);
-        });
-    }
-    isAlive() {
-        if (this.client.connected) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    this.client.on('error', (err) => {
+      console.error(`Redis client not connected to the server: ${err}`);
+    });
+  }
 
-    async get(key) {
-        try {
-            const value = await this.client.getAsync(key);
-            return value;
-        } catch (err) {
-            console.error(err);
-            throw err;
-        }
+  isAlive() {
+    if (this.client.connected) {
+      return true;
     }
+    return false;
+  }
 
-    async set(key, value, duration) {
-      try {
-        const result = await this.client.setExAsync(key, duration, value);
-        return result
-      } catch (err) {
-        console.error(err);
-        throw err;
-      }  
+  async get(key) {
+    try {
+      const value = await this.client.getAsync(key);
+      return value;
+    } catch (err) {
+      console.error(err);
+      throw err;
     }
+  }
 
-    async del(key) {
-        try {
-            const delResult = await this.client.delAsync(key);
-            return delResult;
-        } catch (err) {
-            console.error(err);
-            throw err;
-        }
+  async set(key, value, duration) {
+    try {
+      const result = await this.client.setExAsync(key, duration, value);
+      return result;
+    } catch (err) {
+      console.error(err);
+      throw err;
     }
+  }
+
+  async del(key) {
+    try {
+      const delResult = await this.client.delAsync(key);
+      return delResult;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
 }
 
 const redisClient = new RedisClient();
